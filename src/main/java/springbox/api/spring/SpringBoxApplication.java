7dd3b9859528d6050246b276cd59dca2c242306f
BBanshy <belcastromathias15@gmail.com>
2023-04-16 16:23:40 +0200

    Re init

---
+++ src/main/java/springbox/api/spring/SpringBoxApplication.java
@@ -0,0 +1,13 @@
+package springbox.api.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springbox/api/spring/SpringBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package springbox.api.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
